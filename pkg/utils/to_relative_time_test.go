f5b4c408ed636295c897bc1b6cafe13c45d1d144
Ryan Bennett <ryanbennettvoid@gmail.com>
2024-04-12 20:03:26 -0700

    move timestamp generation outside of ToRelativeTime

--- pkg/utils/to_relative_time_test.go
+++ pkg/utils/to_relative_time_test.go
@@ -8,24 +8,26 @@ import (
 
 func TestToRelativeTime(t *testing.T) {
 
+	now := time.Now()
+
 	{
-		pastTime := time.Now().Add(-time.Second * 5)
-		output := ToRelativeTime(pastTime)
+		pastTime := now.Add(-time.Second * 5)
+		output := ToRelativeTime(pastTime, now)
 		assert.Equal(t, "5s", output)
 	}
 	{
-		pastTime := time.Now().Add(-time.Hour * 6)
-		output := ToRelativeTime(pastTime)
+		pastTime := now.Add(-time.Hour * 6)
+		output := ToRelativeTime(pastTime, now)
 		assert.Equal(t, "6h", output)
 	}
 	{
-		pastTime := time.Now().Add(-time.Hour * 24 * 7)
-		output := ToRelativeTime(pastTime)
+		pastTime := now.Add(-time.Hour * 24 * 7)
+		output := ToRelativeTime(pastTime, now)
 		assert.Equal(t, "7d", output)
 	}
 	{
-		pastTime := time.Now().Add(-time.Hour * 24 * 800)
-		output := ToRelativeTime(pastTime)
+		pastTime := now.Add(-time.Hour * 24 * 800)
+		output := ToRelativeTime(pastTime, now)
 		assert.Equal(t, "800d", output)
 	}
 
