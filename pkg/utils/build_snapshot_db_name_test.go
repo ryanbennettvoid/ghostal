8fa819bed1bb82bba1c05cb5d9343c92dcc31eb0
Ryan Bennett <ryanbennettvoid@gmail.com>
2024-04-12 19:58:16 -0700

    move timestamp generation outside of BuildSnapshotDBName

--- pkg/utils/build_snapshot_db_name_test.go
+++ pkg/utils/build_snapshot_db_name_test.go
@@ -9,15 +9,15 @@ import (
 )
 
 func TestBuildSnapshotDBName_ValidInput(t *testing.T) {
-	input := "niceapp"
-	output := BuildSnapshotDBName(input)
+	inputName := "niceapp"
+	inputTimestamp := time.Now()
+	output := BuildSnapshotDBName(inputName, inputTimestamp)
 	parts := strings.Split(output, "_")
 	assert.Len(t, parts, 4)
 	partsWithoutTimestamp := parts[:3]
 	assert.Equal(t, []string{"ghostel", "snapshot", "niceapp"}, partsWithoutTimestamp)
 
-	// NOTE: comparing timestamps has a non-zero chance of random failure
 	timestampStr := parts[3]
-	nowUnixStr := fmt.Sprintf("%d", time.Now().Unix())
-	assert.Equalf(t, nowUnixStr[:9], timestampStr[:9], "first 9 digits of timestamp match current time")
+	nowUnixStr := fmt.Sprintf("%d", inputTimestamp.UnixMilli())
+	assert.Equal(t, nowUnixStr, timestampStr)
 }
