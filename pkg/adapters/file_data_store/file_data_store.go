07d72efe8fd9822ac33490a7a991a16cf037e599
Ryan Bennett <11631739+ryanbennettvoid@users.noreply.github.com>
2024-04-19 00:59:30 -0700

    climb parent directories to find config file

--- pkg/adapters/file_data_store/file_data_store.go
+++ pkg/adapters/file_data_store/file_data_store.go
@@ -1,7 +1,9 @@
 package file_data_store
 
 import (
+	"ghostal/pkg/values"
 	"os"
+	"path"
 )
 
 type FileDataStore struct {
@@ -12,15 +14,29 @@ func NewFileDataStore(filepath string) *FileDataStore {
 	return &FileDataStore{filepath: filepath}
 }
 
-func (d *FileDataStore) Load() ([]byte, error) {
-	data, err := os.ReadFile(d.filepath)
+func (d *FileDataStore) loadFilepath(fp string) ([]byte, error) {
+	data, err := os.ReadFile(fp)
 	if err != nil {
-		if os.IsNotExist(err) {
-			return make([]byte, 0), nil
-		}
 		return nil, err
 	}
 	return data, nil
+}
+
+func (d *FileDataStore) Load() ([]byte, error) {
+	filepath := d.filepath
+	for depth := 0; depth < values.ConfigScanClimbMaxDepth; depth++ {
+		data, err := d.loadFilepath(filepath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				filepath = path.Join("../", filepath)
+				continue
+			}
+			return nil, err
+		}
+		return data, nil
+	}
+	// file not found after climbing parent directories
+	return make([]byte, 0), nil
 }
 
 func (d *FileDataStore) Save(data []byte) error {
--- pkg/values/constants.go
+++ pkg/values/constants.go
@@ -2,3 +2,4 @@ package values
 
 const DefaultConfigFilepath = ".ghostal"
 const SnapshotDBPrefix = "ghostalsnapshot_"
+const ConfigScanClimbMaxDepth = 50
