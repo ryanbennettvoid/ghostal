de0d22aa1798be7f2ebcfb579efea29fa5c93691
Ryan Bennett <11631739+ryanbennettvoid@users.noreply.github.com>
2024-04-19 01:11:26 -0700

    fix path resolution for both save and load

--- pkg/adapters/file_data_store/file_data_store.go
+++ pkg/adapters/file_data_store/file_data_store.go
@@ -14,31 +14,44 @@ func NewFileDataStore(filepath string) *FileDataStore {
 	return &FileDataStore{filepath: filepath}
 }
 
-func (d *FileDataStore) loadFilepath(fp string) ([]byte, error) {
-	data, err := os.ReadFile(fp)
-	if err != nil {
-		return nil, err
-	}
-	return data, nil
-}
-
-func (d *FileDataStore) Load() ([]byte, error) {
+func (d *FileDataStore) resolveFilepath() (string, error) {
 	filepath := d.filepath
 	for depth := 0; depth < values.ConfigScanClimbMaxDepth; depth++ {
-		data, err := d.loadFilepath(filepath)
+		_, err := os.Stat(filepath)
 		if err != nil {
 			if os.IsNotExist(err) {
 				filepath = path.Join("../", filepath)
 				continue
 			}
-			return nil, err
+			return "", err
 		}
-		return data, nil
+		return filepath, nil
 	}
-	// file not found after climbing parent directories
-	return make([]byte, 0), nil
+	// if not found, use the original filepath
+	return d.filepath, nil
+}
+
+func (d *FileDataStore) Load() ([]byte, error) {
+	filepath, err := d.resolveFilepath()
+	if err != nil {
+		return nil, err
+	}
+	data, err := os.ReadFile(filepath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			// if not exists, return empty data
+			return make([]byte, 0), nil
+		}
+		// if any other type of error, return error
+		return nil, err
+	}
+	return data, nil
 }
 
 func (d *FileDataStore) Save(data []byte) error {
-	return os.WriteFile(d.filepath, data, 0644)
+	filepath, err := d.resolveFilepath()
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filepath, data, 0644)
 }
